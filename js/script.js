485468b003c4937b065b93140607f6302f535fd8
Moumi Guha <guha.moumi.20@gmail.com>
2019-02-10 17:23:34 +0530

    Create-blog done.

--- js/script.js
+++ js/script.js
@@ -3,4 +3,14 @@ $(document).ready(function () {
 
         $('#search-text').toggle(250);
     });
+
+    $('#titleInput').click(function(){
+        var heightFromTop = $('#titleInput').position().top + 50;
+        $('#media-icon').css('top', heightFromTop);
+    });
+
+    $('#write').click(function(){
+        var heightFromTop = $('#write').position().top + 7;
+        $('#media-icon').css('top', heightFromTop);
+    });
 });
